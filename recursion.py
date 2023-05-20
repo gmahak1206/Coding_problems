a4aa209614b0af49a2b49978ee8421a630e81594
Mahak gupta <120080656+gmahak1206@users.noreply.github.com>
2023-05-21 00:26:23 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,3 +1,4 @@
+#printing numbers 1 to n
 def num(n):
     if n == 1:
         print(1,end=" ")
@@ -5,30 +6,35 @@ def num(n):
     num(n-1)
     print(n , end=" ")
 
+#printing numbers n to 1
 def num_rev(n):
     if n == 0:
         return 
     print(n , end = " ")
     num_rev(n-1)
 
+#printing array
 def array(arr , i):
     if i == len(arr):
         return 
     print(arr[i] , end = " ")
     array(arr , i+1)
 
+#printing array in reverse sequence
 def array_rev(arr , i):
     if i < 0:
         return 
     print(arr[i], end = " ")
     array_rev(arr , i-1)
 
+#finding maximum in a array
 def maxa2(arr , i):
     if i == len(arr)-1:
         return arr[i]
     maxa = max(arr[i] , maxa2(arr , i+1))
     return maxa
 
+#finding minimum in a array
 def mina2(arr , i):
     if i == len(arr)-1:
         return arr[i]
